83715978d2fe0a0e7a65d09fe132da87d28cd85d
iamzhiyudong <zhiyudong@163.com>
2022-06-25 22:32:57 +0800

    feat: add hotreload module;

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/astaxie/beego v1.12.3 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-ini/ini v1.66.6 // indirect
--- main.go
+++ main.go
@@ -3,24 +3,39 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
+	"syscall"
 
+	"github.com/fvbock/endless"
 	"github.com/iamzhiyudong/go-gin-example/pkg/setting"
 	"github.com/iamzhiyudong/go-gin-example/routers"
 )
 
 func main() {
-	router := routers.InitRouter()
+	// *** 添加热更新之前启动形式
+	// router := routers.InitRouter()
 
-	// 实例化一个 http 客户端结构体，绑定处理 handler
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:        router,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20, // 请求头的最大字节数
+	// // 实例化一个 http 客户端结构体，绑定处理 handler
+	// s := &http.Server{
+	// 	Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+	// 	Handler:        router,
+	// 	ReadTimeout:    setting.ReadTimeout,
+	// 	WriteTimeout:   setting.WriteTimeout,
+	// 	MaxHeaderBytes: 1 << 20, // 请求头的最大字节数
+	// }
+	// s.ListenAndServe()
+
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+
+	server := endless.NewServer(endPoint, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
 
-	s.ListenAndServe()
-	log.Fatal(2, "ListenAndServe: %v", "123")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
 }
